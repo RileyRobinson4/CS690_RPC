147a61a2550d77ced874ea77299722d4e088c49a
Riley Robinson <RileyRobinson4@gmail.com>
2023-11-12 22:15:43 -0600

    Simplifed server

--- client/msg_proc.c
+++ client/msg_proc.c
@@ -16,16 +16,6 @@ printmessage_1_svc(msg, req)
 	char **msg;	
 	struct svc_req *req;		/* details of call */
 {
-	static int result;			/* must be static! */
-	FILE *f;
- 
-	f = fopen("/dev/console", "w");
-	if (f == (FILE *)NULL) {
-		result = 0;
-		return (&result);
-	}
-	fprintf(f, "%s\n", *msg);
-	fclose(f);
-	result = 1;
-	return (&result);
+	printf("%s\n", msg);
+	return(1);
 }
